a310b51d79578d89561e19560063f6556a1600d0
LemuelKessler <lemuel.carvalho@shopee.com>
2026-05-20 18:41:44 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      // Ignora os erros do ESLint durante o build na Vercel
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  module.exports = nextConfig;
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
